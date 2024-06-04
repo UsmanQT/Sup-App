cd98a317f2b8688af7bf9285c93d2d44b0736e0d
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-04 17:02:27 -0400

    added background image set to fillbounds content scale

--- app/src/main/java/com/example/sup/MainActivity.kt
+++ app/src/main/java/com/example/sup/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.sup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sup.ui.theme.SupTheme
 
@@ -31,7 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SupAppLayout() {
-
+    val backgroundImage = painterResource(id = R.drawable.whatsapp_background)
+    Image(
+        painter = backgroundImage,
+        contentDescription = null,
+        contentScale = ContentScale.FillBounds)
 }
 
 @Preview(showBackground = true)
