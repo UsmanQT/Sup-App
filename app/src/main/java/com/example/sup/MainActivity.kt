57961a31854eabebb0d6a1d8cfad09c6116f3134
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-05 13:40:23 -0400

    stretched out the background image on the whole screen and added the textfield at the bottom

--- app/src/main/java/com/example/sup/MainActivity.kt
+++ app/src/main/java/com/example/sup/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sup.ui.theme.SupTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,12 +43,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SupAppLayout() {
+fun SupAppLayout(modifier: Modifier = Modifier) {
     val backgroundImage = painterResource(id = R.drawable.whatsapp_background)
-    Image(
-        painter = backgroundImage,
-        contentDescription = null,
-        contentScale = ContentScale.FillBounds)
+    Box(modifier = Modifier
+        .fillMaxSize()) {
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+        Column(
+            modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom
+        ){
+            TextField(
+                value = "", onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
