cf65d06811b3134588ce45d8194ad3337837e754
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-05 14:03:37 -0400

    added card to cover the backgrounnd of the textfield

--- app/src/main/java/com/example/sup/MainActivity.kt
+++ app/src/main/java/com/example/sup/MainActivity.kt
@@ -3,22 +3,31 @@ package com.example.sup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,25 +54,42 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SupAppLayout(modifier: Modifier = Modifier) {
     val backgroundImage = painterResource(id = R.drawable.whatsapp_background)
-    Box(modifier = Modifier
-        .fillMaxSize()) {
+    Box() {
         Image(
             painter = backgroundImage,
             contentDescription = null,
-            contentScale = ContentScale.FillBounds,
+            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        Column(
-            modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom
-        ){
-            TextField(
-                value = "", onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            )
-        }
+        MessageWritingArea(modifier = modifier)
+    }
+}
 
+@Composable
+fun MessageWritingArea(modifier: Modifier =  Modifier) {
+    Box (modifier = modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter) {
+        Card(modifier = modifier) {
+            Row(
+                modifier = modifier
+            ) {
+                TextField(
+                    value = "", onValueChange = {},
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .border(
+                            BorderStroke(
+                                width = 1.dp,
+                                color = Color.Gray,
+                            )
+                        ),
+
+                    )
+            }
+        }
     }
 }
 
