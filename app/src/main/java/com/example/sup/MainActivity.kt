0209e8000f342ec0fc0603eccaae7bcc11ba0ec7
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-05 16:05:11 -0400

    added send button when the textfield is not empty

--- app/src/main/java/com/example/sup/MainActivity.kt
+++ app/src/main/java/com/example/sup/MainActivity.kt
@@ -1,36 +1,27 @@
 package com.example.sup
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.sharp.AccountBox
 import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material.icons.sharp.Call
+import androidx.compose.material.icons.sharp.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -72,6 +63,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SupAppLayout(modifier: Modifier = Modifier) {
     val backgroundImage = painterResource(id = R.drawable.whatsapp_background)
+
+    val mutableListSender = mutableListOf("");
+
     Box {
         Image(
             painter = backgroundImage,
@@ -105,6 +99,10 @@ fun MessageWritingArea(modifier: Modifier =  Modifier) {
     var message by remember {
         mutableStateOf("")
     }
+    
+    var textFieldEmpty by remember {
+        mutableStateOf(true)
+    }
 
     Box (modifier = modifier
         .fillMaxSize(),
@@ -126,7 +124,15 @@ fun MessageWritingArea(modifier: Modifier =  Modifier) {
 
                 }
                 TextField(
-                    value = message , onValueChange = {message = it},
+                    value = message , onValueChange = {
+                        message = it
+                        if (message.isNotEmpty()) {
+                            textFieldEmpty = false
+                        }
+                        else {
+                            textFieldEmpty = true
+                        }
+                                                      },
                     textStyle = TextStyle(color = Color.Black),
                     shape = RoundedCornerShape(20.dp),
                     modifier = Modifier
@@ -140,26 +146,41 @@ fun MessageWritingArea(modifier: Modifier =  Modifier) {
                         unfocusedContainerColor = Color.White,
                     )
                 )
-                IconButton(
-                    onClick = { /*TODO*/ },
-                ) {
-                    Icon(
-                        Icons.Sharp.AccountBox,
-                        "add document",
-                        modifier = modifier.size(30.dp)
-                    )
+                when(textFieldEmpty) {
+                    true -> Row {
+                        IconButton(
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Icon(
+                                Icons.Sharp.AccountBox,
+                                "add document",
+                                modifier = modifier.size(30.dp)
+                            )
 
-                }
-                IconButton(
-                    onClick = { /*TODO*/ },
-                ) {
-                    Icon(
-                        Icons.Sharp.Call,
-                        "add document",
-                        modifier = modifier.size(25.dp)
-                    )
+                        }
+                        IconButton(
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Icon(
+                                Icons.Sharp.Call,
+                                "add document",
+                                modifier = modifier.size(25.dp)
+                            )
 
+                        }
+                    }
+                    false -> IconButton(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Icon(
+                            Icons.Sharp.Send,
+                            "add document",
+                            modifier = modifier.size(25.dp)
+                        )
+
+                    }
                 }
+                
             }
         }
     }
