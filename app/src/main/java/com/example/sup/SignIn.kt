64ea4ef2ded6b7083ea985c3875b22ba7cda6c05
usmanqt <qureshiu@mail.gvsu.edu>
2024-06-10 15:46:33 -0400

    basic sign in page designed

--- app/src/main/java/com/example/sup/SignIn.kt
+++ app/src/main/java/com/example/sup/SignIn.kt
@@ -1,20 +1,72 @@
 package com.example.sup
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 
 fun SignInScreen(modifier: Modifier = Modifier) {
+    var emailText by remember {
+        mutableStateOf("")
+    }
+    var passwordText by remember {
+        mutableStateOf("")
+    }
     Scaffold {
         innerPadding -> Column (
-            modifier = modifier.padding(innerPadding)
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(innerPadding)
         ){
-            Text(text = "Hey")
+            Text(
+                text = "Sign In",
+                modifier = modifier.padding(bottom = 16.dp),
+                style = MaterialTheme.typography.titleLarge
+            )
+            OutlinedTextField(
+                value = emailText,
+                onValueChange = {emailText = it},
+                label = {
+                    Text(
+                        text = "Email", color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                },
+                modifier = modifier.padding(bottom = 16.dp)
+
+            )
+            OutlinedTextField(
+                value = passwordText,
+                onValueChange = {passwordText = it},
+                label = {
+                    Text(
+                        text = "Password", color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                },
+                modifier = modifier.padding(bottom = 16.dp)
+                )
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Submit")
+             }
         }
     }
 }
